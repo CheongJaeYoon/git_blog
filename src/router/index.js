ea25c51c76570344223f923f46f85269fb472f98
정재윤 <jyjung9910@gmail.com>
2021-04-20 20:07:26 +0900

    add basic component

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Post from '@/components/Post'
+import Home from '@/components/Home'
+import Profile from '@/components/Profile'
 
 Vue.use(Router)
 
@@ -8,8 +10,19 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/post/:category/:postName',
+      name: 'Post',
+      component: Post
+    },
+    {
+      path: '/profile',
+      name: 'Profile',
+      component: Profile
     }
-  ]
+  ],
+  mode: 'history'
 })
