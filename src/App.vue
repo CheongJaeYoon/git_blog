e186c330da3e07a3edb9947aeee53e1ffa9c51be
정재윤 <jyjung9910@gmail.com>
2021-04-22 15:13:35 +0900

    TopBar design

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,110 @@
 <template>
   <div id="app">
-    <router-view/>
+    <header>
+      <top-bar/>
+    </header>
+    <aside>
+      <side-menu/>
+    </aside>
+    <main>
+      <section>
+        <router-view/>
+      </section>
+    </main>
+    <footer>
+      <!-- -->
+    </footer>
   </div>
 </template>
 
 <script>
+import SideMenu from './components/SideMenu.vue'
+import TopBar from './components/TopBar.vue'
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    TopBar,
+    SideMenu
+  },
+  data () {
+    return {
+      firstClick: false
+    }
+  },
+  mounted () {
+    this.initEventListener()
+  },
+  methods: {
+    initEventListener () {
+      document.querySelectorAll('header, main, footer').forEach((el) => {
+        el.addEventListener('click', (event) => {
+          if (document.querySelector('aside').style.left === '0px' && this.firstClick === true) {
+            document.querySelector('aside').style.left = '-300px'
+            this.firstClick = false
+          } else if (document.querySelector('aside').style.left === '0px') {
+            this.firstClick = true
+          }
+        })
+      })
+    }
+  }
 }
 </script>
 
 <style>
+@font-face {
+    font-family: 'KyoboHand';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_20-04@1.0/KyoboHand.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
+}
+html, body {
+    line-height: 1.73em;
+    height: 100%;
+}
+* {
+    font-family : 'KyoboHand', serif;
+    font-weight: bold;
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+}
 #app {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+header {
+  margin-top: 3px;
+  margin-left: 3px;
+  margin-right: 3px;
+  height: 50px;
+}
+main {
+  height: 100%;
+  background-color: white;
+}
+aside {
+  position: absolute;
+  top : 0px;
+  left : 0px;
+  left : -300px;
+  width: 300px;
+  height: 100%;
+  background-color: cadetblue;
+  transition: 0.3s;
+}
+section {
+  position: relative;
+  width: 1000px;
+  height: 100%;
+  margin-left : -500px;
+  left: 50%;
+  background-color: chartreuse;
 }
 </style>
--- src/components/TopBar.vue
+++ src/components/TopBar.vue
@@ -1,6 +1,10 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
+  <div class="topBar">
+    <font-awesome-icon
+      class="fa-2x menu-button"
+      icon="bars"
+      @click="clickButton"/>
+    <div class="title">{{ msg }}</div>
   </div>
 </template>
 
@@ -9,7 +13,12 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'SlowlyToYou의 블로그'
+    }
+  },
+  methods: {
+    clickButton () {
+      document.querySelector('aside').style.left = '0px'
     }
   }
 }
@@ -17,18 +26,23 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  .topBar{
+    height: 100%;
+    border: 5px solid #333333;
+    background-color: white;
+    border-radius: 255px 15px 225px 15px/15px 225px 15px 255px;
+    display: flex;
+    flex-direction: row;
+  }
+  .menu-button{
+    top : 11px;
+    left : 17px;
+    position: absolute;
+    cursor: pointer;
+  }
+  .title{
+    margin-top: 7px;
+    font-size: 30px;
+    width: 100%;
+  }
 </style>
